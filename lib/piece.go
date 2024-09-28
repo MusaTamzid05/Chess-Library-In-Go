9c60e94e0c8e59653930ce39df217c8319288973
MusaTamzid05 <tamzid101@gmail.com>
2024-09-28 22:31:19 +0600

    Add all piece

--- lib/piece.go
+++ lib/piece.go
@@ -64,3 +64,261 @@ func (p Rook) GetValidMoves() []Move {
 }
 
 
+type Knight struct {
+    Common RootPiece
+}
+
+func MakeKnight(cell Cell) Knight {
+    return Knight{Common: RootPiece{Cell: cell}}
+}
+
+func (p Knight) GetCommon() *RootPiece {
+    return &p.Common
+}
+
+
+func (p Knight) GetValidMoves() []Move {
+    moves := []Move{}
+
+    row := p.Common.Cell.Row
+    col := p.Common.Cell.Col
+
+    newRow := row + 2
+    newCol := col + 1
+
+
+    if newRow < 8 && newCol < 8 {
+        moves = append(moves, Move{Row: newRow, Col: newCol})
+    }
+
+
+    newRow = row + 2
+    newCol = col - 1
+
+
+    if newRow < 8 && newCol >= 0 {
+        moves = append(moves, Move{Row: newRow, Col: newCol})
+    }
+
+
+    newRow = row - 2
+    newCol = col - 1
+
+
+    if newRow >= 0 && newCol >= 0 {
+        moves = append(moves, Move{Row: newRow, Col: newCol})
+    }
+
+
+
+    newRow = row - 2
+    newCol = col + 1
+
+
+    if newRow >= 0 && newCol < 8 {
+        moves = append(moves, Move{Row: newRow, Col: newCol})
+    }
+
+    return moves
+}
+
+
+type Bishop struct {
+    Common RootPiece
+}
+
+func MakeBishop(cell Cell) Bishop {
+    return Bishop{Common: RootPiece{Cell: cell}}
+}
+
+func (p Bishop) GetCommon() *RootPiece {
+    return &p.Common
+}
+
+
+func (p Bishop) GetValidMoves() []Move {
+    moves := []Move{}
+
+    row := p.Common.Cell.Row
+    col := p.Common.Cell.Col
+
+    newRow := row
+    newCol := col
+
+    // down, right
+    for true {
+        newRow += 1
+        newCol += 1
+
+        if newRow < 8 && newCol < 8 {
+            moves = append(moves, Move{Row: newRow, Col: newCol})
+        } else {
+            break
+        }
+    }
+
+    newRow = row
+    newCol = col
+
+
+    // up, left
+    for true {
+        newRow -= 1
+        newCol -= 1
+
+        if newRow >= 0  && newCol >= 0  {
+            moves = append(moves, Move{Row: newRow, Col: newCol})
+        } else {
+            break
+        }
+    }
+
+    newRow = row
+    newCol = col
+
+    // down , left
+    for true {
+        newRow += 1
+        newCol -= 1
+
+        if newRow < 8  && newCol >= 0  {
+            moves = append(moves, Move{Row: newRow, Col: newCol})
+        } else {
+            break
+        }
+    }
+
+    // down , right
+    for true {
+        newRow -= 1
+        newCol += 1
+
+        if newRow >= 0  && newCol < 8  {
+            moves = append(moves, Move{Row: newRow, Col: newCol})
+        } else {
+            break
+        }
+    }
+
+
+    return moves
+}
+
+
+type Queen struct {
+    Common RootPiece
+}
+
+func MakeQueen(cell Cell) Queen {
+    return Queen{Common: RootPiece{Cell: cell}}
+}
+
+func (p Queen) GetCommon() *RootPiece {
+    return &p.Common
+}
+
+
+func (p Queen) GetValidMoves() []Move {
+    moves := []Move{}
+
+    rook := MakeRook(p.Common.Cell)
+    moves = append(moves, rook.GetValidMoves()...)
+
+    bishop := MakeBishop(p.Common.Cell)
+    moves = append(moves, bishop.GetValidMoves()...)
+
+    return moves
+}
+
+
+
+
+type King struct {
+    Common RootPiece
+}
+
+func MakeKing(cell Cell) King {
+    return King{Common: RootPiece{Cell: cell}}
+}
+
+func (p King) GetCommon() *RootPiece {
+    return &p.Common
+}
+
+
+func (p King) GetValidMoves() []Move {
+    moves := []Move{}
+
+    row := p.Common.Cell.Row
+    col := p.Common.Cell.Col
+
+    if row - 1 >= 0 {
+        moves = append(moves, Move{Row: row - 1, Col: col})
+
+    }
+
+    if row + 1 < 8 {
+        moves = append(moves, Move{Row: row + 1, Col: col})
+    }
+
+
+    if col - 1 >= 0 {
+        moves = append(moves, Move{Row: row, Col: col - 1})
+
+    }
+
+    if col + 1 < 8 {
+        moves = append(moves, Move{Row: row, Col: col + 1})
+    }
+
+
+    if row - 1 >= 0 && col - 1 >= 0 {
+        moves = append(moves, Move{Row: row - 1, Col: col - 1})
+
+    }
+
+    if row + 1 < 8 && col + 1 < 8 {
+        moves = append(moves, Move{Row: row + 1, Col: col + 1})
+    }
+
+    if row - 1 >= 0 && col + 1 < 8 {
+        moves = append(moves, Move{Row: row - 1, Col: col + 1})
+    }
+
+    if row + 1 < 8 && col - 1 < 8 {
+        moves = append(moves, Move{Row: row + 1, Col: col + 1})
+    }
+
+
+    return moves
+}
+
+
+type Pawn struct {
+    Common RootPiece
+}
+
+func MakePawn(cell Cell) King {
+    return King{Common: RootPiece{Cell: cell}}
+}
+
+func (p Pawn) GetCommon() *RootPiece {
+    return &p.Common
+}
+
+
+func (p Pawn) GetValidMoves() []Move {
+    moves := []Move{}
+
+    row := p.Common.Cell.Row
+    col := p.Common.Cell.Col
+
+    if row - 1 >= 0 {
+        moves = append(moves, Move{Row: row - 1, Col: col})
+
+    }
+
+    return moves
+}
+
+
